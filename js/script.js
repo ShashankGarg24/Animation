21bd94295e5c4a0feea831c971a2edb7ac9bb327
ShashankGarg24 <sg24101999@gmail.com>
2020-08-26 14:38:19 +0530

    Buttons added

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
-var degree1 = 0;
-var looper;
+//background
 
 var i = 0;
 
@@ -10,44 +9,53 @@ function move_bg() {
 }
 setInterval(move_bg, 20);
 
+//right limbs
+var degree1 = 0;
+var loop;
+
 function rotateR(el, speed) {
   var right_leg = document.getElementById(el);
   right_leg.style.transitionDelay = "0.1s";
   right_leg.style.transform = "rotate(" + degree1 + "deg)";
 
-  looper = setTimeout("rotateR('" + el + "'," + speed + ")", speed);
+  loop = setTimeout("rotateR('" + el + "'," + speed + ")", speed);
   degree1++;
   if (degree1 > 1) {
     degree1 = -3;
   }
 }
 
+//left limbs
 var degree2 = 0;
+var loop2;
 
 function rotateL(el, speed) {
   var left_leg = document.getElementById(el);
   left_leg.style.transform = "rotate(" + degree2 + "deg)";
 
-  looper2 = setTimeout("rotateL('" + el + "'," + speed + ")", speed);
-  degree2--;
-  if (degree2 < -1) {
-    degree2 = 3;
+  loop2 = setTimeout("rotateL('" + el + "'," + speed + ")", speed);
+  degree2 -= 0.4;
+  if (degree2 < -1.3) {
+    degree2 = 2;
   }
 }
 
+//goggles
 var degree3 = 0;
+var loop3;
 
 function moveG(el, speed) {
   var goggles = document.getElementById(el);
 
   goggles.style.transform = "rotate(" + degree3 + "deg)";
-  looper3 = setTimeout("moveG('" + el + "'," + speed + ")", speed);
+  loop3 = setTimeout("moveG('" + el + "'," + speed + ")", speed);
   degree3 -= 0.2;
   if (degree3 < -0.4) {
     degree3 = 0.2;
   }
 }
 
+//day and night
 function day() {
   document.body.style.backgroundColor = "rgba(248, 197, 103, 0.7)";
   document.getElementById("buildings").style.backgroundImage =
